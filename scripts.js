bd52ad422d9d473b3e18cfc7ca7bc9ab56b08b31
Prosperity-Mthethwa <nyamboseprosperity@gmail.com>
2024-10-26 22:57:05 +0200

    modified: script.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,25 @@
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
+const body = document.querySelector("body");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  result.innerText = Math.floor(dividend / divider);
+  if (!dividend || !divider) {
+    result.innerText = "Division not performed. both values are required in inputs. Try again";
+    return false; 
+  };
+  if (Number(divider) === 0){
+    result.innerText = "Division not performed. Invalid number provided. Try again";
+    console.error(divider);
+    return false;
+  };
+  const letter = /[a-zA-Z]/;
+  const specialC = /'.!@\$\^()_+]/;
+  if (letter.test(dividend) || specialC.test(dividend) || letter.test(divider) || specialC.test(divider)){
+    body.innerText = "Something critical went wrong. Please reload the page";
+    console.error(entries);
+  };
 });
